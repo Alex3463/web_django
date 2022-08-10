0a7248cfd0775aa84fa2797668c6921722308416
Alex3463 <amdtheg@icloud.com>
2022-08-10 09:13:53 +0900

    카테고리 기능 추가

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestView(TestCase):
+    def test_post_list(self):
+        self.assertEqual(2,2)
